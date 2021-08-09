91581ed8c0156c44eab9f506b806ed04090146f2
Moe Kayali <t-moekayali@microsoft.com>
2021-08-09 10:36:13 -0700

    seperate metalearning harness

---
+++ frameworks/flaml-metalearn/__init__.py
@@ -0,0 +1,25 @@
+from amlb.utils import call_script_in_same_dir
+
+
+def setup(*args, **kwargs):
+    call_script_in_same_dir(__file__, "setup.sh", *args, **kwargs)
+
+
+def run(dataset, config):
+    from frameworks.shared.caller import run_in_venv
+
+    data = dict(
+        train=dict(X=dataset.train.X, y=dataset.train.y),
+        test=dict(X=dataset.test.X, y=dataset.test.y),
+        problem_type=dataset.type.name,
+    )
+    options = dict(serialization=dict(sparse_dataframe_deserialized_format="dense"))
+
+    return run_in_venv(
+        __file__,
+        "exec.py",
+        input_data=data,
+        dataset=dataset,
+        config=config,
+        options=options,
+    )
