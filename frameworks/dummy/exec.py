a8a99eb3aa920f72c69fe834eae9b2777d38db22
Moe Kayali <t-moekayali@microsoft.com>
2021-09-03 13:46:52 -0700

    regret matrix generation

--- frameworks/dummy/exec.py
+++ frameworks/dummy/exec.py
@@ -2,13 +2,14 @@ import logging
 import os
 import pickle
 from pickle import HIGHEST_PROTOCOL
+from time import time
 
 from flaml import AutoML, __version__
 
 from frameworks.shared.callee import call_run, result, output_subdir
 from frameworks.shared.utils import Timer
 
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, log_loss, balanced_accuracy_score
 from lightgbm.basic import LightGBMError
 
 import numpy as np
@@ -29,22 +30,44 @@ def run(dataset, config):
     for file in os.listdir("mined/"):
         with open("mined/" + file, 'rb') as f:
             try:
+                automl = AutoML()
                 M = pickle.load(f, encoding="utf-8")
-                try:
-                    M._n_classes = len(np.unique(dataset.train.y.squeeze()))
-                except:
-                    print(type(M))
-                    pass
-                M.fit(X_train, y_train)
-                P = M.predict_proba(X_test)[:, 1]
-                auc = roc_auc_score(y_test, P)
-                r = open(f"auc-matrix.csv", "a+")
-                r.write(f"{file},{config.name},{auc}\n")
+                if hasattr(M, "params") and "objective" in M.params:
+                    if config['type_'] == "binary":
+                        M.params["objective"] = "binary"
+                    else:
+                        M.params["objective"] = "multiclass"
+                automl.fit(X_train, y_train, max_iter=0)
+                # read from automl task type, set params.
+                M.__class__.init()
+                t1 = time()
+                print(len(automl._X_train_all))
+                M.fit(automl._X_train_all, automl._y_train_all, budget=600)
+                t2 = time()
+                automl._trained_estimator = M
+                if config['type_'] == "binary":
+                    P = automl.predict_proba(X_test)[:, 1]
+                    score = 1 - roc_auc_score(y_test, P)
+                else:
+                    P = automl.predict_proba(X_test)
+                    score = log_loss(y_test, P)
+                r = open(f"regret-matrix.csv", "a+")
+                r.write(f"{file},{config.name},{score}\n")
                 r.close()
+
+                t = open(f"timing-matrix.csv", "a+")
+                t.write(f"{file},{config.name},{t2 - t1}\n")
+                t.close()
             except Exception as e:
+                import traceback
+                print(M.__class__)
+                print(type(M))
+                print(file)
+                print(traceback.format_exc())
                 continue
 
-    return {}
+    import sys
+    sys.exit(0)
 
 
 if __name__ == '__main__':
