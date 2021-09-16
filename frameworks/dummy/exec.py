d82157122259bd68ffeb36c5df84a3625df73c6c
Moe Kayali <kayali@dragon.cs.washington.edu>
2021-09-16 16:47:53 -0700

    updates

--- frameworks/dummy/exec.py
+++ frameworks/dummy/exec.py
@@ -1,18 +1,15 @@
 import logging
-import os
-import pickle
-from pickle import HIGHEST_PROTOCOL
+import json
 from time import time
 
-from flaml import AutoML, __version__
+from flaml import AutoML, __version__, model
 
 from frameworks.shared.callee import call_run, result, output_subdir
 from frameworks.shared.utils import Timer
 
-from sklearn.metrics import roc_auc_score, log_loss, balanced_accuracy_score
-from lightgbm.basic import LightGBMError
 
-import numpy as np
+from sklearn.metrics import roc_auc_score
+from lightgbm.basic import LightGBMError
 
 log = logging.getLogger(__name__)
 
@@ -27,48 +24,39 @@ def run(dataset, config):
         y_train = y_train.cat.codes
         y_test = y_test.cat.codes
 
-    for file in os.listdir("mined/"):
-        with open("mined/" + file, 'rb') as f:
-            try:
-                automl = AutoML()
-                M = pickle.load(f, encoding="utf-8")
-                if hasattr(M, "params") and "objective" in M.params:
-                    if config['type_'] == "binary":
-                        M.params["objective"] = "binary"
-                    else:
-                        M.params["objective"] = "multiclass"
-                automl.fit(X_train, y_train, max_iter=0)
-                # read from automl task type, set params.
-                M.__class__.init()
-                t1 = time()
-                print(len(automl._X_train_all))
-                M.fit(automl._X_train_all, automl._y_train_all, budget=600)
-                t2 = time()
-                automl._trained_estimator = M
-                if config['type_'] == "binary":
-                    P = automl.predict_proba(X_test)[:, 1]
-                    score = 1 - roc_auc_score(y_test, P)
-                else:
-                    P = automl.predict_proba(X_test)
-                    score = log_loss(y_test, P)
-                r = open(f"regret-matrix.csv", "a+")
-                r.write(f"{file},{config.name},{score}\n")
-                r.close()
+    model_json = config.framework_params["model"]
+    with open(model_json, "r") as f:
+        stored = json.load(f)
 
-                t = open(f"timing-matrix.csv", "a+")
-                t.write(f"{file},{config.name},{t2 - t1}\n")
-                t.close()
-            except Exception as e:
-                import traceback
-                print(M.__class__)
-                print(type(M))
-                print(file)
-                print(traceback.format_exc())
-                continue
+    class_ = getattr(model, stored["class"])
+    print(class_)
+    M = class_(**stored["hyperparameters"])
 
-    import sys
-    sys.exit(0)
+    automl = AutoML()
+    if hasattr(M, "params") and "objective" in M.params:
+        if config["type_"] == "binary":
+            M.params["objective"] = "binary"
+        else:
+            M.params["objective"] = "multiclass"
+    automl.fit(X_train, y_train, max_iter=0, keep_search_state=True)
+    # read from automl task type, set params.
+    M.__class__.init()
+    t1 = time()
+    M.fit(automl._X_train_all, automl._y_train_all, budget=config.max_runtime_seconds)
+    t2 = time()
+    automl._trained_estimator = M
+
+    P = automl.predict_proba(X_test)
+    if config["type_"] == "binary":
+        P = P[:, 1]
+    score = 1 - roc_auc_score(y_test, P, multi_class="ovo")
+
+    r = open(config.framework_params["output"], "a+")
+    r.write(f"{model_json},{config.name},{score}\n")
+    r.close()
+
+    return result()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     call_run(run)
--- frameworks/flaml-extract/exec.py
+++ frameworks/flaml-extract/exec.py
@@ -1,6 +1,6 @@
 import logging
 import os
-from pickle import HIGHEST_PROTOCOL
+import json
 
 from flaml import AutoML, __version__
 
@@ -11,68 +11,82 @@ log = logging.getLogger(__name__)
 
 
 def run(dataset, config):
-    log.info(f"\n**** FLAML [v{__version__}] ****\n")
+    log.info(f"\n**** FLAML-extract [v{__version__}] ****\n")
 
     X_train, y_train = dataset.train.X, dataset.train.y.squeeze()
     X_test, y_test = dataset.test.X, dataset.test.y.squeeze()
 
-    is_classification = config.type == 'classification'
+    is_classification = config.type == "classification"
     time_budget = config.max_runtime_seconds
-    n_jobs = config.framework_params.get('_n_jobs', config.cores)
+    n_jobs = config.framework_params.get("_n_jobs", config.cores)
+    print(config)
     log.info("Running FLAML with {} number of cores".format(config.cores))
     aml = AutoML()
 
     # Mapping of benchmark metrics to flaml metrics
     metrics_mapping = dict(
-        acc='accuracy',
-        auc='roc_auc',
-        f1='f1',
-        logloss='log_loss',
-        mae='mae',
-        mse='mse',
-        rmse='rmse',
-        r2='r2',
+        acc="accuracy",
+        auc="roc_auc",
+        auc_ovo="roc_auc_ovo",
+        f1="f1",
+        logloss="log_loss",
+        mae="mae",
+        mse="mse",
+        rmse="rmse",
+        r2="r2",
     )
-    perf_metric = metrics_mapping[
-        config.metric] if config.metric in metrics_mapping else 'auto'
+    perf_metric = (
+        metrics_mapping[config.metric] if config.metric in metrics_mapping else "auto"
+    )
     if perf_metric is None:
         log.warning("Performance metric %s not supported.", config.metric)
 
-    training_params = {k: v for k, v in config.framework_params.items()
-                       if not k.startswith('_')}
+    training_params = {
+        k: v for k, v in config.framework_params.items() if not k.startswith("_")
+    }
 
     log_dir = output_subdir("logs", config)
     flaml_log_file_name = os.path.join(log_dir, "flaml.log")
+    train_limit = (
+        training_params["train_time_limit"]
+        if "train_time_limit" in training_params
+        else float("inf")
+    )
     with Timer() as training:
-        aml.fit(X_train, y_train,
-                metric=perf_metric,
-                task=config.type,
-                n_jobs=n_jobs,
-                log_file_name= flaml_log_file_name,
-                time_budget=time_budget, **training_params)
+        aml.fit(
+            X_train,
+            y_train,
+            metric=perf_metric,
+            task=config.type,
+            n_jobs=n_jobs,
+            log_file_name=flaml_log_file_name,
+            time_budget=time_budget,
+            train_time_limit=train_limit,
+            # **training_params,
+        )
 
-    with open('model.json', 'a+', encoding='utf-8') as f:
-        f.write(str(aml.model.__dict__) + "\n")
-    
-    import pickle
-    with open(f'mined/{config.name}.pickle', 'wb') as f:
-        pickle.dump(aml.model, f, protocol=HIGHEST_PROTOCOL)
-    
+    with open(training_params["json"], "w", encoding="utf-8") as f:
+        j = {
+            "class": type(aml.model).__name__,
+            "hyperparameters": aml.model.params,
+        }
+        json.dump(j, f, ensure_ascii=False, allow_nan=False, indent=4)
+
     with Timer() as predict:
         predictions = aml.predict(X_test)
     probabilities = aml.predict_proba(X_test) if is_classification else None
     labels = aml.classes_ if is_classification else None
-    return result(  
-                    output_file=config.output_predictions_file,
-                    probabilities=probabilities,
-                    predictions=predictions,
-                    truth=y_test,
-                    models_count=len(aml.config_history),
-                    training_duration=training.duration,
-                    predict_duration=predict.duration,
-                    probabilities_labels=labels,
-                )
+    return result(
+        output_file=config.output_predictions_file,
+        probabilities=probabilities,
+        predictions=predictions,
+        truth=y_test,
+        models_count=len(aml.config_history),
+        training_duration=training.duration,
+        predict_duration=predict.duration,
+        probabilities_labels=labels,
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     call_run(run)
